609dd33021595a3503b567ea26544e73ac9e2e34
jade fisher <fisherrjd@gmail.com>
2025-07-10 13:51:18 -0600

    idk

---
+++ lists-frontend/src/components/ListsTable.vue
@@ -0,0 +1,42 @@
+<script setup lang="ts">
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from '@/components/ui/table'
+</script>
+
+<template>
+  <Table>
+    <TableHeader>
+      <TableRow>
+        <TableHead class="text-left w-[150px]">
+          LIST_NAME
+        </TableHead>
+        <TableHead class="text-left w-[150px]">
+          CREATED
+        </TableHead>        
+        <TableHead class="text-left w-[150px]">
+          NUMBER_OF_TASKS
+        </TableHead>
+      </TableRow>
+    </TableHeader>
+    <TableBody>
+      <TableRow>
+        <TableCell class="text-left font-medium">
+          Kroger Groceries
+        </TableCell>
+        <TableCell class="text-left">
+          July 3rd, 2025
+        </TableCell>
+        <TableCell class="text-left">
+          10
+        </TableCell>
+      </TableRow>
+    </TableBody>
+  </Table>
+</template>
--- lists-frontend/src/views/Lists.vue
+++ lists-frontend/src/views/Lists.vue
@@ -1,6 +1,10 @@
+<script setup>
+import ListsTable from '@/components/ListsTable.vue'
+</script>
+
 <template>
   <div class="p-6 text-zinc-900 dark:text-zinc-100">
-    <h1 class="text-xl font-semibold">All Lists</h1>
-    <p>This is all your lists view.</p>
+    <h1 class="text-xl font-semibold">Lists</h1>
+    <ListsTable/>
   </div>
 </template>
